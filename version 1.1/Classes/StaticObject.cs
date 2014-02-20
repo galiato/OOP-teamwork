59902d6bacede9f5ec42f9804d976b29accac0e5
SvetlinSlavchev <s_slavchev@yahoo.com>
2014-02-20 21:20:52 +0200

    first class diagram

---
+++ version 1.1/Classes/StaticObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace version_1._1
+{
+    public abstract class StaticObject : Object
+    {
+    }
+}
