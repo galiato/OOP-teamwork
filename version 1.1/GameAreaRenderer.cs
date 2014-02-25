f5db7edd9a75bfff424a4b07f2060ac7da748b3a
ttitto <ttitto_prog@vfemail.net>
2014-02-25 18:04:56 +0200

    all commit

---
+++ version 1.1/GameAreaRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSmartPenguin
+{
+    class GameAreaRenderer : IRenderer
+    {
+        public void EnqueueForRendering()
+        {
+            //TODO:
+            throw new NotImplementedException();
+        }
+
+        public void RenderAll()
+        {
+            //TODO:
+            throw new NotImplementedException();
+        }
+
+        public void ClearQueue()
+        {
+            //TODO:
+            throw new NotImplementedException();
+        }
+    }
+}
